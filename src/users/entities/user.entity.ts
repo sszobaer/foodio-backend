817c921bab9961cdfc07e827b18bb5a3e3a6a8ba
SSZASeyam <118447914+sszobaer@users.noreply.github.com>
2026-03-12 01:16:58 +0600

    Production/Deployment check

--- src/users/entities/user.entity.ts
+++ src/users/entities/user.entity.ts
@@ -1,1 +1,14 @@
-export class User {}
+import { Entity, PrimaryGeneratedColumn, Column } from "typeorm";
+
+@Entity()
+export class User {
+
+  @PrimaryGeneratedColumn('uuid')
+  id: string;
+
+  @Column({ unique: true })
+  username: string;
+
+  @Column()
+  password: string;
+}
