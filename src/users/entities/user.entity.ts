cce01e8381b5bbaed4f3eda873ca47ea0ab13cf6
SSZASeyam <118447914+sszobaer@users.noreply.github.com>
2026-03-12 01:39:40 +0600

    Db connection check

--- src/users/entities/user.entity.ts
+++ src/users/entities/user.entity.ts
@@ -1,14 +1,10 @@
-import { Entity, PrimaryGeneratedColumn, Column } from "typeorm";
+import { Column, Entity, PrimaryGeneratedColumn } from "typeorm";
 
 @Entity()
 export class User {
-
-  @PrimaryGeneratedColumn('uuid')
-  id: string;
-
-  @Column({ unique: true })
-  username: string;
+  @PrimaryGeneratedColumn()
+  id: number;
 
   @Column()
-  password: string;
+  name: string;
 }
